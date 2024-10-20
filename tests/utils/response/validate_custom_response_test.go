437bff9a18d28018703cd408cbcfdb3fb36c08b4
SamPariatIL <samanbha.pariat@infinitelocus.com>
2024-10-20 18:13:05 +0530

    Added constants

--- tests/utils/response/validate_custom_response_test.go
+++ tests/utils/response/validate_custom_response_test.go
@@ -12,36 +12,47 @@ type ValidateCustomResponseSuite struct {
 }
 
 func (suite *ValidateCustomResponseSuite) TestValidCity() {
+	ok := "ok"
+	created := "created"
+	badRequest := "bad request"
+	unauthorized := "unauthorized"
+	forbidden := "forbidden"
+	notFound := "not found"
+	conflict := "conflict"
+	internalServerError := "internal server error"
+	somethingWentWrong := "something went wrong"
+	noError := ""
+
 	customResponses := []struct {
 		data    any
 		status  int
 		error   string
 		message string
 	}{
-		{data: "ok", status: 200, error: "", message: "ok"},
-		{data: "error", status: 201, error: "", message: "created"},
-		{data: "error", status: 400, error: "something went wrong", message: "bad request"},
-		{data: "error", status: 401, error: "something went wrong", message: "unauthorized"},
-		{data: "error", status: 403, error: "something went wrong", message: "forbidden"},
-		{data: "error", status: 404, error: "something went wrong", message: "not found"},
-		{data: "error", status: 409, error: "something went wrong", message: "conflict"},
-		{data: "error", status: 500, error: "something went wrong", message: "internal server error"},
-		{data: fiber.Map{"message": "ok"}, status: 200, error: "", message: "ok"},
-		{data: fiber.Map{"message": "error"}, status: 201, error: "", message: "created"},
-		{data: fiber.Map{"message": "error"}, status: 400, error: "something went wrong", message: "bad request"},
-		{data: fiber.Map{"message": "error"}, status: 401, error: "something went wrong", message: "unauthorized"},
-		{data: fiber.Map{"message": "error"}, status: 403, error: "something went wrong", message: "forbidden"},
-		{data: fiber.Map{"message": "error"}, status: 404, error: "something went wrong", message: "not found"},
-		{data: fiber.Map{"message": "error"}, status: 409, error: "something went wrong", message: "conflict"},
-		{data: fiber.Map{"message": "error"}, status: 500, error: "something went wrong", message: "internal server error"},
-		{data: nil, status: 200, error: "", message: "ok"},
-		{data: nil, status: 201, error: "", message: "created"},
-		{data: nil, status: 400, error: "something went wrong", message: "bad request"},
-		{data: nil, status: 401, error: "something went wrong", message: "unauthorized"},
-		{data: nil, status: 403, error: "something went wrong", message: "forbidden"},
-		{data: nil, status: 404, error: "something went wrong", message: "not found"},
-		{data: nil, status: 409, error: "something went wrong", message: "conflict"},
-		{data: nil, status: 500, error: "something went wrong", message: "internal server error"},
+		{data: "ok", status: 200, error: noError, message: ok},
+		{data: "error", status: 201, error: noError, message: created},
+		{data: "error", status: 400, error: somethingWentWrong, message: badRequest},
+		{data: "error", status: 401, error: somethingWentWrong, message: unauthorized},
+		{data: "error", status: 403, error: somethingWentWrong, message: forbidden},
+		{data: "error", status: 404, error: somethingWentWrong, message: notFound},
+		{data: "error", status: 409, error: somethingWentWrong, message: conflict},
+		{data: "error", status: 500, error: somethingWentWrong, message: internalServerError},
+		{data: fiber.Map{"message": ok}, status: 200, error: noError, message: ok},
+		{data: fiber.Map{"message": created}, status: 201, error: noError, message: created},
+		{data: fiber.Map{"message": badRequest}, status: 400, error: somethingWentWrong, message: badRequest},
+		{data: fiber.Map{"message": unauthorized}, status: 401, error: somethingWentWrong, message: unauthorized},
+		{data: fiber.Map{"message": forbidden}, status: 403, error: somethingWentWrong, message: forbidden},
+		{data: fiber.Map{"message": notFound}, status: 404, error: somethingWentWrong, message: notFound},
+		{data: fiber.Map{"message": conflict}, status: 409, error: somethingWentWrong, message: conflict},
+		{data: fiber.Map{"message": internalServerError}, status: 500, error: somethingWentWrong, message: internalServerError},
+		{data: nil, status: 200, error: noError, message: ok},
+		{data: nil, status: 201, error: noError, message: created},
+		{data: nil, status: 400, error: somethingWentWrong, message: badRequest},
+		{data: nil, status: 401, error: somethingWentWrong, message: unauthorized},
+		{data: nil, status: 403, error: somethingWentWrong, message: forbidden},
+		{data: nil, status: 404, error: somethingWentWrong, message: notFound},
+		{data: nil, status: 409, error: somethingWentWrong, message: conflict},
+		{data: nil, status: 500, error: somethingWentWrong, message: internalServerError},
 	}
 
 	for _, customResponse := range customResponses {
